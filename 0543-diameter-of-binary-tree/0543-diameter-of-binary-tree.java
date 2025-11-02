501e95aa11dbf22829b2f4d1025ebeb0952d1f89
harshivam <113045030+harshivam@users.noreply.github.com>
2025-11-03 00:40:52 +0530

    Time: 0 ms (100%), Space: 46.7 MB (5.05%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -1,50 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
+public class Solution {
+    int max = 0;
+    
     public int diameterOfBinaryTree(TreeNode root) {
-        if (root == null)
-            return 0;
-
-        int diameterMid = diameter(root);
-        int diameterLeft = diameterOfBinaryTree(root.left);
-        int diameterRight = diameterOfBinaryTree(root.right);
-
-        return Math.max(diameterMid, Math.max(diameterLeft, diameterRight));
-
+        maxDepth(root);
+        return max;
     }
-
-    static int diameter(TreeNode root) {
-        if (root == null)
-            return 0;
-        int diameter = 0;
-        if (root.left != null)
-            diameter++;
-        if (root.right != null)
-            diameter++;
-
-        diameter += height(root.left) + height(root.right);
-
-        return diameter;
-    }
-
-    static int height(TreeNode root) {
-
-        if (root == null || root.left == null && root.right == null)
-            return 0;
-
-        return 1 + Math.max(height(root.left), height(root.right));
+    
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        
+        max = Math.max(max, left + right);
+        
+        return Math.max(left, right) + 1;
     }
 }
