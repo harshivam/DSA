cfba2f0df467b460d622e9772b4f480f79c7daab
harshivam <113045030+harshivam@users.noreply.github.com>
2025-12-03 23:04:02 +0530

    Time: 2647 ms (5%), Space: 42.2 MB (9.84%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,14 +1,18 @@
 class Solution {
     public int numTrees(int n) {
-        return helper(1,n);
+        int[][] memo = new int[n+1][n+1];
+        return helper(1,n,memo);
     }
-    public int helper(int start , int end){
+    public int helper(int start , int end,int[][] memo){
         if(start>=end) return 1;
+        if(memo[start][end]>1){
+            return memo[start][end];
+        }
         int ans = 0;
 
         for(int i = start; i<= end ; i++){
-            int l = helper(start,i-1);
-            int r = helper(i+1, end);
+            int l = helper(start,i-1,memo); //left subtree
+            int r = helper(i+1, end,memo); //right subtree
             ans += l*r;
         }
         return ans;
