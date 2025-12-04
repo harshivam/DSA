db14fc1abb6621f2687e67a27062aa5c2e30cf45
harshivam <113045030+harshivam@users.noreply.github.com>
2025-12-04 13:05:08 +0530

    Time: 1 ms (99.94%), Space: 45.4 MB (40.26%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.java
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -1,6 +1,5 @@
 class Solution {
     List<List<Integer>> list = new ArrayList<>();
-
     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
         ArrayList<Integer> subList = new ArrayList<>();
         sum(root, targetSum, 0, subList);
@@ -8,17 +7,14 @@ class Solution {
     }
 
     public void sum(TreeNode root, int targetSum, int sum, List<Integer> subList) {
-
         if (root == null) {
             return;
         }
         sum += root.val;
         subList.add(root.val);
-
         if (root.left == null && root.right == null && sum == targetSum) {
             list.add(new ArrayList<>(subList));
         }
-
         sum(root.left, targetSum, sum, subList);
         sum(root.right, targetSum, sum, subList);
         subList.remove(subList.size() - 1);
