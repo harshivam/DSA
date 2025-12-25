691ba2eeecaad38b31a2201b3b651b98ab96efe9
harshivam <113045030+harshivam@users.noreply.github.com>
2025-12-26 01:18:30 +0530

    Time: 1 ms (90.87%), Space: 49.1 MB (89.55%) - LeetHub

--- 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.java
@@ -1,45 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode reverseOddLevels(TreeNode root) {
-        Queue<TreeNode> q1 = new LinkedList<>();
-        q1.offer(root);
-        int level = 0;
-        while (!q1.isEmpty()) {
-            int size = q1.size();
-            LinkedList<TreeNode> list = new LinkedList<>();
-            for (int i = 0; i < size; i++) {
-                TreeNode node = q1.poll();
-                list.add(node);
-                if(node.left!=null) q1.offer(node.left);
-                if(node.right!=null) q1.offer(node.right);
-            }
-               if(level%2!=0){
-                int i = 0;
-                int j = list.size()-1;
-                while(i<j){
-                    int temp = list.get(j).val;
-                    list.get(j).val = list.get(i).val;
-                    list.get(i).val = temp;
-                    i++;
-                    j--;
-                }
-               }
-               level++;
-        }
+        dfs(root.left, root.right, 1);
         return root;
+    }
+
+    private void dfs(TreeNode left, TreeNode right, int level) {
+        if (left == null || right == null) return;
+
+        if (level % 2 == 1) {
+            int temp = left.val;
+            left.val = right.val;
+            right.val = temp;
+        }
+
+        dfs(left.left, right.right, level + 1);
+        dfs(left.right, right.left, level + 1);
     }
 }
