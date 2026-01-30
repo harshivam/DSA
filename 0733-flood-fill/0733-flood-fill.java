acbf158baf598af4bd1142cf9c32bfd5670e4eb5
harshivam <113045030+harshivam@users.noreply.github.com>
2026-01-30 13:28:20 +0530

    Time: 0 ms (100%), Space: 47.1 MB (19.76%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,26 +1,24 @@
 class Solution {
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        boolean[][] visited = new boolean[image.length][image[0].length];
         int originalColor = image[sr][sc];
-        recurse(image, sr, sc, color, visited, originalColor);
+        dfs(image, sr, sc, color, originalColor);
         return image;
 
     }
 
-    void recurse(int[][] image, int sr, int sc, int color,boolean[][] visited, int originalColor) {
-        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || visited[sr][sc] == true
-                || image[sr][sc] != originalColor)
+    void dfs(int[][] image, int sr, int sc, int color, int originalColor) {
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || image[sr][sc] != originalColor || image[sr][sc]==color)
             return;
         image[sr][sc] = color;
-        visited[sr][sc] = true;
+    
         // left call 
-        recurse(image, sr, sc - 1, color, visited, originalColor);
+        dfs(image, sr, sc - 1, color,  originalColor);
         // right call 
-        recurse(image, sr, sc + 1, color, visited, originalColor);
+        dfs(image, sr, sc + 1, color, originalColor);
         // top call 
-        recurse(image, sr - 1, sc, color, visited, originalColor);
+        dfs(image, sr - 1, sc, color, originalColor);
         // bottom call 
-        recurse(image, sr + 1, sc, color,  visited, originalColor);
+        dfs(image, sr + 1, sc, color, originalColor);
     }
 }
